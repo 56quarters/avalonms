3aaf5a84abdb3fd8cac913c2e77c72cf8b90ba97
Nick Pillitteri <nick@tshlabs.org>
2012-04-11 20:31:47 -0400

    Finish insert service

---
+++ avalonms/services.py
@@ -0,0 +1,93 @@
+# -*- coding: utf-8 -*-
+#
+
+
+"""
+"""
+
+
+from avalonms.models import (
+    Album,
+    Artist,
+    Genre,
+    Track)
+
+
+__all__ = []
+
+
+class InsertService(object):
+
+    """
+    """
+
+    def __init__(self, scanned, session):
+        """
+        """
+        self._scanned = scanned
+        self._session = session
+        self._cache = {
+            'album': {},
+            'artist': {},
+            'genre': {}
+            }
+
+    def _get_id(self, field, val):
+        """
+        """
+        return self._cache[field][val].id
+
+    def _load_relations(self):
+        """
+        """
+        insert = []
+        
+        for tag in self._scanned:
+            album = self._populate_relation(tag, 'album', Album)
+            artist = self._populate_relation(tag, 'artist', Artist)
+            genre = self._populate_relation(tag, 'genre', Genre)
+
+            if album is not None:
+                insert.append(album)
+            if artist is not None:
+                insert.append(artist)
+            if genre is not None:
+                insert.append(genre)
+
+        self._session.add_all(insert)
+        self._session.commit()
+
+
+    def _populate_relation(self, tag, field, model_cls):
+        """
+        """
+        cache = self._cache[field]
+        val = getattr(tag, field)
+
+        if val in cache:
+            return None
+
+        obj = model_cls()
+        obj.name = val
+        cache[val] = obj
+        return obj
+
+    def insert_tracks(self):
+        """
+        """
+        self._load_relations()
+        insert = []
+
+        for tag in self._scanned:
+            track = Track()
+            track.name = tag.title
+            track.track = tag.track
+            track.year = tag.year
+
+            track.album_id = self._get_id('album', tag.album)
+            track.artist_id = self._get_id('artist', tag.artist)
+            track.genre_id = self._get_id('genre', tag.genre)
+
+            insert.append(track)
+        self._session.add_all(insert)
+        self._session.commit()
--- test.py
+++ test.py
@@ -2,29 +2,9 @@
 
 import os.path
 
-import avalonms.scan
 import avalonms.models
-
-
-root ='music/audio'
-#root  ='music/audio/CIVET'
-files = avalonms.scan.get_files(
-    os.path.join(os.path.expanduser('~'), root))
-tags = avalonms.scan.get_tags(files)
-
-
-artists = set()
-albums = set()
-genres = set()
-tracks = []
-
-
-for data in tags.values():
-    track = avalonms.models.Track()
-    track.name = data.title
-    track.track = data.track
-    track.year = data.year
-    tracks.append(track)
+import avalonms.scan
+import avalonms.services
 
 
 handler = avalonms.models.SessionHandler()
@@ -32,5 +12,11 @@ handler.connect()
 handler.create_tables()
 
 sess = handler.get_session()
-sess.add_all(tracks)
-sess.commit()
+
+root = 'music/audio'
+files = avalonms.scan.get_files(
+    os.path.join(os.path.expanduser('~'), root))
+tags = avalonms.scan.get_tags(files)
+
+service = avalonms.services.InsertService(tags.values(), sess)
+service.insert_tracks()
