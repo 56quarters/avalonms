f04596f0b227c2c69268b0438eefdbe92ddb9778
Nick Pillitteri <nick@tshlabs.org>
2012-04-19 00:37:33 -0400

    Test script cleanup

--- tests/generate_urls.py
+++ tests/generate_urls.py
@@ -1,4 +1,5 @@
 #!/usr/bin/env python
+# -*- coding: utf-8 -*-
 #
 
 
@@ -11,6 +12,7 @@ to the songs endpoint
 from __future__ import print_function
 
 import os
+import os.path
 import sys
 import urllib
 
