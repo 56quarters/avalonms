16c68a35d9d21bdc603b496bf418a675f4b822e7
Nick Pillitteri <nick@tshlabs.org>
2013-01-06 21:27:28 -0500

    Rename test directory

--- test/test_tag_read.py
+++ test/test_tag_read.py
@@ -3,11 +3,36 @@
 
 import unittest
 
+import mox
+import pytest
+
+import avalon.tags.read
+
+
+class TagpyMock(object):
+    def FileRef(self, path):
+        pass
+
+
+class MutagenMock(object):
+    def File(self, path, easy=True):
+        pass
+
 
 class TestReadTagpy(unittest.TestCase):
 
+    def setup_method(self, method):
+        self.mox = mox.Mox()
+        self.tagpy = self.mox.CreateMock(TagpyMock)
+
     def test_unicode_error(self):
-        pass
+        self.tagpy.FileRef(mox.IsA(unicode)).AndRaise(UnicodeDecodeError("Could not do something"))
+        self.mox.ReplayAll()
+
+        with pytest.raises(IOError):
+            avalon.tags.read.read_tagpy(unicode('/path/file'), self.tagpy)
+
+        self.mox.VerifyAll()
 
     def test_value_error(self):
         pass
