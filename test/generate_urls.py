345323edb5123ca5bc912f9597dfcde6994057fb
Nick Pillitteri <nick@tshlabs.org>
2013-01-11 12:36:31 -0500

    More expansive URL generation

--- test/generate_urls.py
+++ test/generate_urls.py
@@ -44,31 +44,41 @@ artist_base = 'http://localhost:8080/avalon/artists?'
 genre_base = 'http://localhost:8080/avalon/genres?'
 
 session = handle.get_session()
-urls = []
+urls = set()
 
 for album in session.query(avalon.models.Album).all():
-    urls.append(song_base + 'album=' + urllib.quote_plus(album.name.encode('utf-8')))
-    urls.append(song_base + 'album=' + urllib.quote_plus(album.name.lower().encode('utf-8')))
-    urls.append(song_base + 'album=' + urllib.quote_plus(album.name.upper().encode('utf-8')))
-    urls.append(song_base + 'query=' + urllib.quote_plus(album.name.upper().encode('utf-8')))
-    urls.append(song_base + 'album_id=' + urllib.quote_plus(str(album.id)))
-    urls.append(album_base + 'order=name' )
+    urls.add(song_base + 'album=' + urllib.quote_plus(album.name.encode('utf-8')))
+    urls.add(song_base + 'album=' + urllib.quote_plus(album.name.lower().encode('utf-8')))
+    urls.add(song_base + 'album=' + urllib.quote_plus(album.name.upper().encode('utf-8')))
+    urls.add(song_base + 'album_id=' + urllib.quote_plus(str(album.id)))
+    urls.add(album_base + 'order=name' )
+    for term in album.name.split():
+        urls.add(album_base + 'query=' + urllib.quote_plus(term.encode('utf-8')))
+        urls.add(song_base + 'query=' + urllib.quote_plus(term.encode('utf-8')))
 
 for artist in session.query(avalon.models.Artist).all():
-    urls.append(song_base + 'artist=' + urllib.quote_plus(artist.name.encode('utf-8')))
-    urls.append(song_base + 'artist=' + urllib.quote_plus(artist.name.lower().encode('utf-8')))
-    urls.append(song_base + 'artist=' + urllib.quote_plus(artist.name.upper().encode('utf-8')))
-    urls.append(song_base + 'query=' + urllib.quote_plus(artist.name.encode('utf-8')))
-    urls.append(song_base + 'artist_id=' + urllib.quote_plus(str(artist.id)))
-    urls.append(artist_base + 'order=name')
+    urls.add(song_base + 'artist=' + urllib.quote_plus(artist.name.encode('utf-8')))
+    urls.add(song_base + 'artist=' + urllib.quote_plus(artist.name.lower().encode('utf-8')))
+    urls.add(song_base + 'artist=' + urllib.quote_plus(artist.name.upper().encode('utf-8')))
+    urls.add(song_base + 'artist_id=' + urllib.quote_plus(str(artist.id)))
+    urls.add(artist_base + 'order=name')
+    for term in artist.name.split():
+        urls.add(artist_base + 'query=' + urllib.quote_plus(term.encode('utf-8')))
+        urls.add(song_base + 'query=' + urllib.quote_plus(term.encode('utf-8')))
 
 for genre in session.query(avalon.models.Genre).all():
-    urls.append(song_base + 'genre=' + urllib.quote_plus(genre.name.encode('utf-8')))
-    urls.append(song_base + 'genre=' + urllib.quote_plus(genre.name.lower().encode('utf-8')))
-    urls.append(song_base + 'genre=' + urllib.quote_plus(genre.name.upper().encode('utf-8')))
-    urls.append(song_base + 'query=' + urllib.quote_plus(genre.name.upper().encode('utf-8')))
-    urls.append(song_base + 'genre_id=' + urllib.quote_plus(str(genre.id)))
-    urls.append(genre_base + 'order=name')
+    urls.add(song_base + 'genre=' + urllib.quote_plus(genre.name.encode('utf-8')))
+    urls.add(song_base + 'genre=' + urllib.quote_plus(genre.name.lower().encode('utf-8')))
+    urls.add(song_base + 'genre=' + urllib.quote_plus(genre.name.upper().encode('utf-8')))
+    urls.add(song_base + 'genre_id=' + urllib.quote_plus(str(genre.id)))
+    urls.add(genre_base + 'order=name')
+    for term in genre.name.split():
+        urls.add(genre_base + 'query=' + urllib.quote_plus(term.encode('utf-8')))
+        urls.add(song_base + 'query=' + urllib.quote_plus(term.encode('utf-8')))
+
+for song in session.query(avalon.models.Track).all():
+    for term in song.name.split():
+        urls.add(song_base + 'query=' + urllib.quote_plus(term.encode('utf-8')))
 
 handle.close(session)
 
