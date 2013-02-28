5676887556760d9224c64f66a236f5a6dfab93f0
Nick Pillitteri <nick@tshlabs.org>
2013-02-27 20:13:19 -0500

    Test for insert errors for songs with unicode paths

--- test/test_tag_insert.py
+++ test/test_tag_insert.py
@@ -95,12 +95,12 @@ class TestTrackFieldLoader(object):
 
     def test_insert_invalid_attribute_error(self):
         tag = MockTag()
-        tag.path = unicode('/home/something/music/song.flac')
-        tag.album = unicode('True North')
-        tag.artist = unicode('Bad Religion')
-        tag.genre = unicode('Punk')
+        tag.path = u'/home/something/music/song.flac'
+        tag.album = u'True North'
+        tag.artist = u'Bad Religion'
+        tag.genre = u'Punk'
         tag.length = 115
-        tag.title = unicode('True North')
+        tag.title = u'True North'
         tag.track = 1
         tag.year = 2013
 
@@ -114,8 +114,25 @@ class TestTrackFieldLoader(object):
         self.mox.VerifyAll()
 
 
-    def test_insert_invalid_attribute_error_utf8_path(self):
-        pass
+    def test_insert_invalid_attribute_error_unicode_path(self):
+        tag = MockTag()
+        tag.path = u'/home/something/music/¡Tré!/song.flac'
+        tag.album = u'¡Tré!'
+        tag.artist = u'Green Day'
+        tag.genre = u'Punk'
+        tag.length = 148
+        tag.title = u'Amanda'
+        tag.track = 8
+        tag.year = 2012
+
+        session_handler = self.mox.CreateMock(avalon.models.SessionHandler)
+        model_cls = self.mox.CreateMockAnything(avalon.models.Album)
+
+        inserter = avalon.tags.insert.TrackFieldLoader(session_handler, [tag])
+
+        with pytest.raises(AttributeError):
+            inserter.insert(model_cls, id_gen_mock, 'blah')
+        self.mox.VerifyAll()
 
     def test_insert_duplicate_ids(self):
         pass
