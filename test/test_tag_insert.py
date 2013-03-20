5d1dd8000f3d475ca316e6a93f97dc37ae029ba9
Nick Pillitteri <nick@tshlabs.org>
2013-03-19 23:09:43 -0400

    More tests for the tag field loader

--- test/test_tag_insert.py
+++ test/test_tag_insert.py
@@ -56,7 +56,8 @@ class TestCleaner(object):
     def teardown_method(self, method):
         self.mox.UnsetStubs()
 
-    def test_clean_without_error(self):
+    def test_clean_success(self):
+        """Test that the happy path works properly."""
         session_handler = self.mox.CreateMock(avalon.models.SessionHandler)
         session = self.mox.CreateMock(DummySession)
         query = self.mox.CreateMock(DummyQuery)
@@ -73,6 +74,9 @@ class TestCleaner(object):
         self.mox.VerifyAll()
 
     def test_clean_with_error(self):
+        """Test that errors during cleaning are handled correctly and
+        we don't leak database connections.
+        """
         session_handler = self.mox.CreateMock(avalon.models.SessionHandler)
         session = self.mox.CreateMock(DummySession)
         err = sqlalchemy.exc.SQLAlchemyError()
@@ -97,6 +101,9 @@ class TestTrackFieldLoader(object):
         self.mox.UnsetStubs()
 
     def test_insert_invalid_attribute_error(self):
+        """Test that a call to insert a bogus tag field results in
+        the expected attribute error.
+        """
         tag = MockTag()
         tag.path = u'/home/something/music/song.flac'
         tag.album = u'True North'
@@ -116,8 +123,11 @@ class TestTrackFieldLoader(object):
             inserter.insert(model_cls, id_gen_mock, 'blah')
         self.mox.VerifyAll()
 
-
     def test_insert_invalid_attribute_error_unicode_path(self):
+        """Test that we don't encounter unicode related errors when
+        trying to handle expected errors during insert for tracks with
+        non-ascii characters in the field path.
+        """
         tag = MockTag()
         tag.path = u'/home/something/music/¡Tré!/song.flac'
         tag.album = u'¡Tré!'
@@ -138,6 +148,9 @@ class TestTrackFieldLoader(object):
         self.mox.VerifyAll()
 
     def test_insert_duplicate_ids(self):
+        """Test that fields are deduplicated based on their UUIDs
+        when being inserted into the database.
+        """
         tag1 = MockTag()
         tag1.path = u'/home/something/music/¡Tré!/song.flac'
         tag1.album = u'¡Tré!'
@@ -172,7 +185,7 @@ class TestTrackFieldLoader(object):
         id_gen(mox.IsA(unicode)).AndReturn('b6c4bfc6-c088-48af-a1c0-7bcb3f37035c')
 
         session_handler.get_session().AndReturn(session)
-        session.add_all(mox.IsA(list))
+        session.add_all(mox.Or(mox.In(model1), mox.In(model2)))
         session.commit()
         session_handler.close(session)
 
@@ -185,7 +198,74 @@ class TestTrackFieldLoader(object):
         self.mox.VerifyAll()
 
     def test_insert_commit_error(self):
-        pass
+        """Test that errors inserting fields do not result in
+        leaking database connections.
+        """
+        tag = MockTag()
+        tag.path = u'/home/something/music/song.flac'
+        tag.album = u'True North'
+        tag.artist = u'Bad Religion'
+        tag.genre = u'Punk'
+        tag.length = 115
+        tag.title = u'True North'
+        tag.track = 1
+        tag.year = 2013
+
+        session_handler = self.mox.CreateMock(avalon.models.SessionHandler)
+        session = self.mox.CreateMock(DummySession)
+        id_gen = self.mox.CreateMockAnything(id_gen_mock)
+        model_cls = self.mox.CreateMockAnything(avalon.models.Album)
+        model = self.mox.CreateMock(avalon.models.Album)
+
+        model_cls().AndReturn(model)
+        id_gen(mox.IsA(unicode)).AndReturn('b6c4bfc6-c088-48af-a1c0-7bcb3f37035c')
+
+        session_handler.get_session().AndReturn(session)
+        session.add_all([model])
+        session.commit().AndRaise(sqlalchemy.exc.NoSuchColumnError)
+        session_handler.close(session)
+
+        self.mox.ReplayAll()
+
+        inserter = avalon.tags.insert.TrackFieldLoader(session_handler, [tag])
+
+        # The error is expected, we just want to test that the
+        # session is still closed when there is an insert error
+        with pytest.raises(sqlalchemy.exc.NoSuchColumnError):
+            inserter.insert(model_cls, id_gen, 'album')
+        self.mox.VerifyAll()
+
+    def test_insert_success(self):
+        """Test the happy path for inserting a tag field."""
+        tag = MockTag()
+        tag.path = u'/home/something/music/¡Tré!/song.flac'
+        tag.album = u'¡Tré!'
+        tag.artist = u'Green Day'
+        tag.genre = u'Punk'
+        tag.length = 148
+        tag.title = u'Amanda'
+        tag.track = 8
+        tag.year = 2012
+
+        session_handler = self.mox.CreateMock(avalon.models.SessionHandler)
+        session = self.mox.CreateMock(DummySession)
+        id_gen = self.mox.CreateMockAnything(id_gen_mock)
+        model_cls = self.mox.CreateMockAnything(avalon.models.Album)
+        model = self.mox.CreateMock(avalon.models.Album)
+
+        model_cls().AndReturn(model)
+        id_gen(mox.IsA(unicode)).AndReturn('b6c4bfc6-c088-48af-a1c0-7bcb3f37035c')
+
+        session_handler.get_session().AndReturn(session)
+        session.add_all([model])
+        session.commit()
+        session_handler.close(session)
+
+        self.mox.ReplayAll()
+
+        inserter = avalon.tags.insert.TrackFieldLoader(session_handler, [tag])
+        inserter.insert(model_cls, id_gen, 'album')
+        self.mox.VerifyAll()
 
 
 class TestTrackLoader(object):
