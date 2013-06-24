c2623950b8c548f43edd3a8460d78ebfdfdee342
Nick Pillitteri <nick@tshlabs.org>
2013-06-23 20:07:05 -0400

    Unit tests for album ID cache lookups

--- test/test_cache.py
+++ test/test_cache.py
@@ -1,21 +1,13 @@
 # -*- coding: utf-8 -*-
 #
 
+import uuid
+
 import mox
 import pytest
 
 import avalon.cache
 import avalon.models
-
-
-class MockSession(object):
-    def query(self, cls):
-        pass
-
-
-def MockQueryResult(object):
-    def all(self):
-        pass
 
 
 class TestFunctions(object):
@@ -37,5 +29,59 @@ class TestIdLookupCache(object):
 
     def teardown_method(self, method):
         self.mox.UnsetStubs()
+
+    def test_get_album_id_exists(self):
+        model1 = self.mox.CreateMock(avalon.models.Album)
+        model1.id = uuid.UUID("2d24515c-a459-552a-b022-e85d1621425a")
+        model1.name = u'Dookie'
+
+        model2 = self.mox.CreateMock(avalon.models.Album)
+        model2.id = uuid.UUID("b3c204e4-445d-5812-9366-28de6770c4e1")
+        model2.name = u'Insomniac'
+
+        dao = self.mox.CreateMock(avalon.cache.ReadOnlyDao)
+        dao.get_all_albums().AndReturn([model1, model2])
+        dao.get_all_artists().AndReturn([])
+        dao.get_all_genres().AndReturn([])
+
+        self.mox.ReplayAll()
+
+        cache = avalon.cache.IdLookupCache(dao)
+
+        assert uuid.UUID("2d24515c-a459-552a-b022-e85d1621425a") == cache.get_album_id(u'Dookie')
+        assert uuid.UUID("b3c204e4-445d-5812-9366-28de6770c4e1") == cache.get_album_id(u'Insomniac')
+
+    def test_get_album_id_does_not_exist(self):
+        dao = self.mox.CreateMock(avalon.cache.ReadOnlyDao)
+        dao.get_all_albums().AndReturn([])
+        dao.get_all_artists().AndReturn([])
+        dao.get_all_genres().AndReturn([])
+
+        self.mox.ReplayAll()
+
+        cache = avalon.cache.IdLookupCache(dao)
+
+        assert None is cache.get_album_id(u'Dookie')
+
+    def test_get_album_id_case_insensitive(self):
+        model1 = self.mox.CreateMock(avalon.models.Album)
+        model1.id = uuid.UUID("2d24515c-a459-552a-b022-e85d1621425a")
+        model1.name = u'Dookie'
+
+        model2 = self.mox.CreateMock(avalon.models.Album)
+        model2.id = uuid.UUID("b3c204e4-445d-5812-9366-28de6770c4e1")
+        model2.name = u'Insomniac'
+
+        dao = self.mox.CreateMock(avalon.cache.ReadOnlyDao)
+        dao.get_all_albums().AndReturn([model1, model2])
+        dao.get_all_artists().AndReturn([])
+        dao.get_all_genres().AndReturn([])
+
+        self.mox.ReplayAll()
+
+        cache = avalon.cache.IdLookupCache(dao)
+
+        assert uuid.UUID("2d24515c-a459-552a-b022-e85d1621425a") == cache.get_album_id(u'DOOKIE')
+        assert uuid.UUID("b3c204e4-445d-5812-9366-28de6770c4e1") == cache.get_album_id(u'INSOMNIAC')
 
 
