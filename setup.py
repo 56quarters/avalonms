a93fdff2e008be7e79067b1c686e19784d9e2e52
Nick Pillitteri <nick@tshlabs.org>
2014-10-13 22:19:42 -0400

    Add Python 3.3 and 3.4 to supported versions

--- setup.py
+++ setup.py
@@ -32,7 +32,9 @@ CLASSIFIERS = [
     "Topic :: Multimedia :: Sound/Audio",
     "Programming Language :: Python",
     "Programming Language :: Python :: 2.6",
-    "Programming Language :: Python :: 2.7"
+    "Programming Language :: Python :: 2.7",
+    "Programming Language :: Python :: 3.3",
+    "Programming Language :: Python :: 3.4"
 ]
 
 
